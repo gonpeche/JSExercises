156374cc9d735f7a94e100ed6462c99f24514e2f
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-09-12 18:54:02 -0300

    Update Random.js

--- Random.js
+++ Random.js
@@ -243,5 +243,120 @@ HashTable.prototype.hash = function (val) {
     return hash % this.numBuckets // lo reparte entre 35 lugares
 }
 
+// SELECTORES
+
+var traverseDomAndCollectElements = function(matchFunc, startEl) {
+  var resultSet = [];
+  var prueba = []
+
+  var cont = 0;
+  
+  if (typeof startEl === "undefined") {
+
+    startEl = document.body;
+    // resultSet = [startEl]
+  }
+
+  if (matchFunc(startEl)) {
+  
+    resultSet.push(startEl)
+  } else if (startEl.children) {
+
+    for (var i = 0; i < startEl.children.length; i++) {
+      resultSet = resultSet.concat(
+        traverseDomAndCollectElements(matchFunc,startEl.children[i])
+      )
+    }
+
+  }
+  
+  // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
+  // usa matchFunc para identificar elementos que matcheen
+  return resultSet; // este valor lo toma ELEMENTS
+};
+
+var $ = function(selector) { // entra BODY
+  var elements;
+  var selectorMatchFunc = matchFunctionMaker(selector); // false????
+  elements = traverseDomAndCollectElements(selectorMatchFunc);
+  return elements;
+};
+
+//selectorMathcFunc = function (a) {
+//      return a.tagName.toLowerCase() === selector (i.e: body)
+//    }
+
+
+// Detecta y devuelve el tipo de selector
+// devuelve uno de estos tipos: id, class, tag.class, tag
+var selectorTypeMatcher = function(selector) {
+    if (selector.startsWith('#')) {
+      return 'id'
+    } else if (selector.startsWith('.')) {
+      return 'class'
+    } else if (!selector.includes('.') && !selector.includes('#')) {
+      return 'tag'
+    } else {
+      return 'tag.class'
+    }
+
+};
+
+// NOTA SOBRE LA FUNCIÓN MATCH
+// recuerda, la función matchFunction devuelta toma un elemento como un
+// parametro y devuelve true/false dependiendo si el elemento
+// matchea el selector.
+
+var matchFunctionMaker = function(selector) { // recibe #price
+  var selectorType = selectorTypeMatcher(selector); // #price --> 'id'
+  var matchFunction;
+  if (selectorType === "id") {
+    // define matchFunction para id
+
+      var check = selector.replace(/[^\w\s]/gi, '')
+      return function (a) {
+         return a.id.includes(check)
+      }
+      
+  } else if (selectorType === "class") {
+
+    var check = selector.replace(/[^\w\s]/gi, '')
+    return function (a) {
+      var toArray = a.className.split(' ');
+      for (var i = 0; i < toArray.length; i++) {
+        if (check === toArray[i]) {
+          return true
+        }
+      }
+      return false
+    }
+
+  } else if (selectorType === "tag.class") {
+    var check = selector.replace(/[^\w\s]/gi, ' ').split(' ')
+    return function (a) {
+      var toArray = a.className.split(' ');
+      for (var i = 0; i < toArray.length; i++) {
+        if (check[1] === toArray[i] && check[0] === a.tagName.toLowerCase()) {
+          return true
+        }
+      }
+      return false
+    }
+
+  } else if (selectorType === "tag") {
+    var check = selector.replace(/[^\w\s]/gi, '')
+
+    return function (a) {
+      return a.tagName.toLowerCase() === check
+    }
+
+    // define matchFunction para tag
+
+  }
+  return matchFunction; // toma un elemento como parámetro y devuelve T or F
+};
+
+
+
 
 
